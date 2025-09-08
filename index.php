8a8fb0fea114f01898babbcb93eb6bdf7a9b5cbb
Luis Gustavo Barbosa de Melo <l10921526@gmail.com>
2025-09-08 08:20:19 -0300

    primeiro commit
    primeiro commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Luis Gustavo and Campel" />
+    <meta name="description" content="Atividade em dupla de UDA" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src=""></script>
+  </head>
+  <body>
+    <header>
+      <h1>Colaborando trabalhos em git!</h1>
+      <h2><?php echo"Testando o git hub!" ?></h2>
+    </header>
+    <main>
+      <section id="comandosGit">
+        <h2>Aqui é um dos principais comandos git:</h2>
+        <ul>
+          <li>git init</li>
+          <li>git remote</li>
+          <li>git add</li>
+          <li>git commit -m</li>
+          <li>git push</li>
+        </ul>
+      </section>
+    </main>
+    <footer>
+      <p>Copyright &copy; Luis gustavo and Felipe Campeol</p>
+    </footer>
+  </body>
+</html>
